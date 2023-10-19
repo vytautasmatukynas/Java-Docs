8a667e705479396888450cb5f69d696425adda2e
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-10-19 17:38:12 +0300

    Add files via upload

--- JAVA_DEFAULT/basic_tools/004-ARRAY/ArraysClass.java
+++ JAVA_DEFAULT/basic_tools/004-ARRAY/ArraysClass.java
@@ -7,6 +7,10 @@ public class ArraysClass {
 	public static void main(String[] args) {
 
 		Random random = new Random();
+		
+		emptyArray();
+		arrayMatrix();
+		arrayWithVariables();
 
 		// Create new array
 		int[] arrayA = new int[5];
@@ -47,6 +51,45 @@ public class ArraysClass {
 		// returns index number of first array items that mismatch
 		compareArrayElements(arrayA, arrayB);
 
+	}
+	
+	private static void arrayMatrix() {
+		// 2D array
+//		int [][] matrica = new int[4][4];;
+		int[][] matrica = { { 5, 6, 7, 8 }, { 5, 6, 7, 8 }, { 5, 6, 7, 8 }, { 5, 6, 7, 8 } };
+
+		System.out.println(matrica[1][2]);
+
+		for (int y = 0; y < matrica.length; y++) {
+			for (int x = 0; x < matrica[0].length; x++) {
+				System.out.print(matrica[x][y]);
+			}
+		}
+	}
+
+	private static void arrayWithVariables() {
+		// this will create list with String
+		String[] text2 = { "zzzz", "aaaa", "rrrr" };
+		// print list items in one line
+		for (String i : text2)
+			System.out.print(i + " ");
+
+		System.out.print("\n");
+
+		// change value of second list item
+		text2[1] = "oooo";
+		for (String i : text2)
+			System.out.println(i);
+	}
+
+	private static void emptyArray() {
+		// Can define list fixed length
+		// this will create list with 'null'
+		int[] skaiciai = new int[6];
+		// change value of first list item
+		skaiciai[0] = 5;
+		for (int i : skaiciai)
+			System.out.println(i);
 	}
 
 	private static void compareArrayElements(int[] arrayA, int[] arrayB) {
