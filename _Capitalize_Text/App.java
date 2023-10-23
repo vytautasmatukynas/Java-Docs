fe98b91faece64e933e1f7614c3cf2fc438578a2
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-10-23 11:47:26 +0300

    add

---
+++ _Capitalize_Text/App.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Optional;
+
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println(capitalizeText("kazkas kazkur kazkodel"));
+		System.out.println(capitalizeText("kazkas kazkur"));
+		System.out.println(capitalizeText("kazkas"));
+	}
+
+	public static String capitalizeText(String name) {
+		if (name.contains(" ")) {
+	        Optional<String> formattedText = Arrays.stream(name.split(" "))
+										            .map(word -> Character.toUpperCase(word.charAt(0)) 
+										            			 + word.substring(1))
+										            .reduce((a, b) -> a + " " + b);
+	        
+	        return formattedText.orElse("");
+
+		} else {
+			String formattedText = Character.toUpperCase(name.charAt(0)) 
+									 + name.substring(1);
+
+			return formattedText;
+		}	
+	}
+
+}
