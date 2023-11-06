8e37da52be215464974811284dff81f76205c3b3
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-11-06 14:18:09 +0200

    Update App.java

--- _JAVA_CODE_SNIPPETS/Capitalize_Text/App.java
+++ _JAVA_CODE_SNIPPETS/Capitalize_Text/App.java
@@ -14,21 +14,25 @@ public class App {
 		System.out.println(capitalizeText("kazkas"));
 	}
 
-	public static String capitalizeText(String name) {
-		if (name.contains(" ")) {
-	        Optional<String> formattedText = Arrays.stream(name.split(" "))
-						    .map(word -> Character.toUpperCase(word.charAt(0)) 
-									 + word.substring(1))
-						    .reduce((a, b) -> a + " " + b);
-	        
-	        return formattedText.orElse("");
+	public static String capitalize(String name) {
+		try {
+			if (name.contains(" ")) {
+				Optional<String> formattedText = Arrays.stream(name.split(" "))
+						.map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
+						.reduce((a, b) -> a + " " + b);
 
-		} else {
-			String formattedText = Character.toUpperCase(name.charAt(0)) 
-									 + name.substring(1);
+				return formattedText.orElse("");
 
-			return formattedText;
-		}	
+			} else {
+				String formattedText = Character.toUpperCase(name.charAt(0)) + name.substring(1);
+
+				return formattedText;
+			}
+		} catch (Exception e) {
+			FileHandler.saveToFile(e.getMessage(), "errors/TextEditor_Errors.txt");
+		}
+		
+		return "";
 	}
 
 }
