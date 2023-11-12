f5cb63d73f3d23c1aa52bbbda5641b1101eb54c4
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-11-12 19:02:37 +0200

    add

--- WEB_DEV/Spring/Spring_1st_default/3-CRUD_annotations/controllers/FirstController.java
+++ WEB_DEV/Spring/Spring_1st_default/3-CRUD_annotations/controllers/FirstController.java
@@ -3,32 +3,36 @@ package com.codeacademy.javau7.crud.controllers;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/greeting")
+@RequestMapping("/api/greetings")
 public class FirstController {
-    
+
+    // This method handles HTTP GET requests to "/api/greetings/get"
     @GetMapping("/get")
     public String getGreetingParam() {
         return "GET";
     }
 
+    // This method handles HTTP POST requests to "/api/greetings/add"
     @PostMapping("/add")
     public String postGreeting() {
         return "ADD";
     }
 
+    // This method handles HTTP DELETE requests to "/api/greetings/delete"
     @DeleteMapping("/delete")
     public String delGreetingParams() {
         return "DELETE";
     }
 
+    // This method handles HTTP PATCH requests to "/api/greetings/update/patch"
     @PatchMapping("/update/patch")
     public String patchGreeting() {
         return "PATCH";
     }
 
+    // This method handles HTTP PUT requests to "/api/greetings/update/put"
     @PutMapping("/update/put")
     public String putGreeting() {
         return "PUT";
     }
-
 }
