918ee7f576d8641c995f394af6e9b01d74fcfe74
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-11-12 20:34:30 +0200

    update

--- JAVA_CODE_SNIPPETS/SPRING_Costum_Error_Handler/errorHandler/ApiError.java
+++
@@ -1,8 +0,0 @@
-package lt.codeacademy.javau7.Biblioteka.errorHandler;
-
-import org.springframework.http.HttpStatus;
-
-import java.util.List;
-
-public record ApiError(HttpStatus status, String message, List<String> errors) { }
-
