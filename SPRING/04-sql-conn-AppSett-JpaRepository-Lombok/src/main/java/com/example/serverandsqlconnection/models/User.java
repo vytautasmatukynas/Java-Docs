945268f6ddfeee758150bb854907c888975aa67f
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-11-17 08:47:47 +0200

    updated

--- SPRING/04-sql-conn-AppSett-JpaRepository-Lombok/src/main/java/com/example/serverandsqlconnection/models/User.java
+++ SPRING/04-sql-conn-AppSett-JpaRepository-Lombok/src/main/java/com/example/serverandsqlconnection/models/User.java
@@ -2,20 +2,49 @@ package com.example.serverandsqlconnection.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /*
- * The @Entity annotation marks this class as a JPA entity, representing a table in a relational database.
- *
- * By default, each entity class maps a database table with the same name in the default schema of
- * database. You can customize this mapping using the name, schema, and catalog attributes of the @Table annotation.
- * "@Table(name="user", schema="testdb")"
- *
- * The @Data annotation from Lombok generates getters, setters, toString, equals, hashCode,
- * and a constructor with all non-null fields.
- *
- * The @NoArgsConstructor annotation generates empty constructor.
- * The @AllArgsConstructor annotation generates full constructor.
+    The @Entity annotation marks this class as a JPA entity, representing a table in a relational database.
+
+    By default, each entity class maps a database table with the same name in the default schema of
+    database. You can customize this mapping using the name, schema, and catalog attributes of the @Table annotation.
+    "@Table(name="user", schema="testdb")"
+
+    @Getter and @Setter:
+        Description: Automatically generates getter and setter methods for class properties. For example, placing @Getter on a private property will create a public getter for that property.
+
+    @NoArgsConstructor, @RequiredArgsConstructor, and @AllArgsConstructor:
+        Description:
+            @NoArgsConstructor creates an empty constructor.
+            @RequiredArgsConstructor generates a constructor with final or @NonNull annotated properties.
+            @AllArgsConstructor creates a constructor with all class properties.
+
+    @Data:
+        Description: A composite annotation that includes @Getter, @Setter, @RequiredArgsConstructor,
+        * @ToString, and @EqualsAndHashCode. It provides comprehensive functionality for data classes.
+
+    @Builder:
+        Description: Creates a convenient and safe way to build objects using the Builder design pattern.
+        * Allows creating objects by setting only the desired properties instead of all at once.
+
+    @Slf4j:
+        Description: Automatically generates a Logger object using the SLF4J (Simple Logging Facade for Java)
+        * library, simplifying logging in classes.
+
+    @Value:
+        Description: Creates an immutable class. Similar to @Data, but all properties are final, and only getters
+        * will be generated.
+
+    @NonNull:
+        Description: Used in conjunction with other Lombok annotations, such as @Setter or @RequiredArgsConstructor,
+        * to automatically check if a property is not null. If the property is null, a NullPointerException is thrown.
+
+    @Cleanup:
+        Description: Automatically generates try-finally blocks, ensuring that resources (e.g., streams)
+        * are properly closed.
  */
 @Entity
 @Table(name = "user")
