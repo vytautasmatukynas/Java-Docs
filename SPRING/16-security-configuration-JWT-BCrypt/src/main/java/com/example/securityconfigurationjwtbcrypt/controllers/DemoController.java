a6447f7b6102b145d098f87ba215339bc23caaff
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-11-19 21:50:43 +0200

    updated

--- SPRING/16-security-configuration-JWT-BCrypt/src/main/java/com/example/securityconfigurationjwtbcrypt/controllers/DemoController.java
+++ SPRING/16-security-configuration-JWT-BCrypt/src/main/java/com/example/securityconfigurationjwtbcrypt/controllers/DemoController.java
@@ -1,26 +1,41 @@
 package com.example.securityconfigurationjwtbcrypt.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-/**
- * Controller class for handling demo API requests.
- */
 @RestController
 @RequestMapping("/api/demo")
 @RequiredArgsConstructor
 public class DemoController {
 
     /**
-     * Handles a GET request to the demo API endpoint.
+     * Handles GET requests to "/api/demo" endpoint.
      *
-     * @return A ResponseEntity containing a greeting message.
+     * @return ResponseEntity with a greeting message if the user has either "ADMIN" or "USER" role.
+     * @throws ResponseStatusException with HttpStatus.FORBIDDEN if the user does not have sufficient privileges.
      */
     @GetMapping
     public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello, you are secure here");
+        // Retrieve the current authentication information
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // Check if the user has either "ADMIN" or "USER" role
+        if (authentication.getAuthorities().stream().anyMatch(r -> r.getAuthority().equals("ADMIN") ||
+                r.getAuthority().equals("USER"))) {
+
+            // User has sufficient privileges, return a greeting message
+            return ResponseEntity.ok("Hello, you are secure here");
+
+        } else {
+            // User does not have sufficient privileges, throw Forbidden status exception
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied. Insufficient privileges.");
+        }
     }
 }
